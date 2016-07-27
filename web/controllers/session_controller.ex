d45e1747ee8d648bc8986147417e58c616b7d6ae
SirError <dark_shadow_goth@hotmail.com>
2016-07-27 00:16:08 -0300

    final auth

--- web/controllers/session_controller.ex
+++ web/controllers/session_controller.ex
@@ -14,7 +14,13 @@ defmodule Rumbl.SessionController do
       {:error, _reason, conn} ->
         conn
         |> put_flash(:error, "Invalid username/password")
-        |> render("new.html")             
+        |> render("new.html")
     end
+  end
+
+  def delete(conn, _) do
+    conn
+    |> Rumbl.Auth.logout()
+    |> redirect(to: page_path(conn, :index))
   end
 end
